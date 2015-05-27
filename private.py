e47306bb5e66e296878c641e0c7d3a4d5255695a
medalhkr <medalhkr@gmail.com>
2015-05-27 15:47:55 +0900

    add gitignore for __pycache load config; private

--- private.py
+++ private.py
@@ -2,8 +2,9 @@ from urllib.request import urlopen
 import json
 import threading
 import signal
+import os
+import configfile
 
-sergeant_address = 'http://172.17.0.106/'
 
 class Private(object):
 
@@ -13,7 +14,7 @@ class Private(object):
         self.put_interval = 1
 
         # join
-        response = self.getJson(sergeant_address + 'pvt/join')
+        response = self.getJson(conf.sgtaddr, conf.stgport, 'pvt/join')
         self.soldier_id = response['id']
         # order
         self.askOrder()
@@ -21,7 +22,7 @@ class Private(object):
         self.putValue()
 
     def askOrder(self):
-        response = self.getJson(sergeant_address + 'pvt/order')
+        response = self.getJson(conf.sgtaddr, conf.stgport, 'pvt/order')
         order = response['order']
         if order in self.order_functions:
             result = self.order_functions[order](response)
@@ -31,7 +32,7 @@ class Private(object):
 
     def putValue(self):
         value = "test_value"
-        self.getJson(sergeant_address + 'pvt/put?' + value)
+        self.getJson(conf.sgtaddr, conf.sgtport, 'pvt/put?' + value)
 
         t = threading.Timer(self.put_interval, self.putValue)
         t.start()
@@ -41,12 +42,31 @@ class Private(object):
         self.put_interval = values['value']
 
 
-    def getJson(self, url):
-        res_raw = urlopen(url)
+    def getJson(self, host, port, api):
+        res_raw = urlopen("http://${0}:${1}/${2}".format(host, port, api))
         res_str = res_raw.read().decode('utf-8')
         return json.loads(res_str)
 
+# if __name__ == '__main__':
+conf = configfile.Config()
 
 if __name__ == '__main__':
+    # set config-file name
+    conf_name = ''
+    if len(sys.argv) == 2:
+        conf_name = sys.argv[1]
+    else:
+        script_path = os.path.abspath(os.path.dirname(__file__))
+        conf_name = script_path + '/conf/private.conf'
+
+    # load and check config
+    conf.loadfile(conf_name)
+    if conf.loadconfig(conf_name) == False: quit(1)
+    if hasattr(conf, 'sgtaddr') and \
+       hasattr(conf, 'sgtport'): pass
+    else:
+        print("error: load config file failed; lack of parameter.")
+        quit(1)
+
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     pvt = Private()
