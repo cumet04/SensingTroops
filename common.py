aa618df6adf42cd138c5aabeee0918ffd59e42a2
medalhkr <medalhkr@gmail.com>
2016-02-11 23:36:43 +0900

    [fix] APIエラー時の戻り値のフォーマットが統一されていなかったのを修正

--- common.py
+++ common.py
@@ -15,13 +15,13 @@ def get_dict():
 
     # content-type check
     if request.headers['Content-Type'] != 'application/json':
-        return jsonify(res='application/json required'), 406
+        return jsonify(msg='application/json required'), 406
 
     # json parse
     try:
         param_str = request.data.decode('utf-8')
         param_dict = json.loads(param_str)
     except json.JSONDecodeError:
-        return jsonify(error="param couldn't decode to json"), 400
+        return jsonify(msg="param couldn't decode to json"), 400
 
     return param_dict, 200
