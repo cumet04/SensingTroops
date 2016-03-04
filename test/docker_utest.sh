c69110beeeb79588dfaedac99a8d38e0bad553fa
medalhkr <medalhkr@gmail.com>
2016-03-04 22:05:19 +0900

    [add] dockerのunitテストにテストコマンドを追加

--- test/docker_utest.sh
+++ test/docker_utest.sh
@@ -14,10 +14,14 @@ rsync -a --delete test work_tmp
 function dockerrun() {
     local id=$(docker run -d --expose 50000-53000 -v $PWD/work_tmp:/opt/app/ medalhkr/troops $@)
     local addr=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $id)
-    sleep 0.3
     echo $addr
 }
 
 cpt_addr=$(dockerrun python troops/captain.py $cpt_port)
 sgt_addr=$(dockerrun python troops/sergeant.py $sgt_port $cpt_addr $cpt_port)
 pvt_addr=$(dockerrun python troops/private.py $pvt_port $sgt_addr $sgt_port)
+
+docker run -tv $PWD/work_tmp:/opt/app/ medalhkr/troops \
+    sh -c "cd test; jasmine-node --config addr $pvt_addr --config port $pvt_port spec/private_unit_spec.js"
+docker run -tv $PWD/work_tmp:/opt/app/ medalhkr/troops \
+    sh -c "cd test; jasmine-node --config addr $sgt_addr --config port $sgt_port spec/sergeant_unit_spec.js"
