307e278761c2f41fcab7907efdc801f1fae4c427
medalhkr <medalhkr@gmail.com>
2016-04-06 00:15:58 +0900

    [add] 新しいバージョンのBEANクラス定義を追加

--- troops/common.py
+++ troops/common.py
@@ -8,9 +8,54 @@ from collections import namedtuple
 from flask import jsonify, request
 from werkzeug.exceptions import BadRequest
 
-PrivateInfo = namedtuple('PrivateInfo', ['id', 'name', 'addr', 'port', 'sensors'])
-SergeantInfo = namedtuple('SergeantInfo', ['id', 'name', 'addr', 'port'])
-CaptainInfo = namedtuple('CaptainInfo', ['id', 'name', 'addr', 'port'])
+SoldierInfo = namedtuple('SoldierInfo', [
+        'id',
+        'name',
+        'endpoint',
+        'weapons',
+        'orders'
+    ])
+LeaderInfo = namedtuple('LeaderInfo', [
+        'id',
+        'name',
+        'endpoint',
+        'subordinates',
+        'missions'
+    ])
+CommanderInfo = namedtuple('CommanderInfo', [
+        'id',
+        'name',
+        'endpoint',
+        'subordinates',
+        'campaigns'
+    ])
+
+Campaign = namedtuple('Campaign', [
+        'author',
+        'requirements',
+        'trigger',
+        'place',
+        'purpose',
+        'destination'
+    ])
+
+Mission = namedtuple('Mission', [
+        'author',
+        'requirements',
+        'trigger',
+        'place',
+        'purpose',
+        'destination'
+    ])
+
+Order = namedtuple('Order', [
+        'author',
+        'requirements',
+        'trigger',
+        'place',
+        'purpose',
+        'destination'
+    ])
 
 
 def generate_info(cls, **kwargs):
