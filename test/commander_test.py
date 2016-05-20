82aa26b8ca68a06453e9855c78270b5ef80d676f
medalhkr <medalhkr@gmail.com>
2016-05-20 19:24:38 +0900

    [clean] テストケースの順番を整理
    実装/未実装がわかりにくかったため。整理後、swagger-specに出る順番でテストが記述されるようにした

--- test/commander_test.py
+++ test/commander_test.py
@@ -34,6 +34,7 @@ class CommanderTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+# [GET] /
     def test_get_info(self):
         response = self.app.get('/commander', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
@@ -46,103 +47,6 @@ class CommanderTestCase(unittest.TestCase):
                                   campaigns=[])
         expected = {"result": "success", "info": asdict(commander)}
 
-        self.assertEqual(actual, expected)
-
-# [POST] /subordinates
-    def test_add_subordinate(self):
-        leader = LeaderInfo(id='lxxx0',
-                            name='cmd_http',
-                            endpoint='http://localhost:50000',
-                            subordinates=[],
-                            missions=[])
-        response = self.app.post('/commander/subordinates',
-                                 data=json.dumps(asdict(leader)),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        actual = json.loads(response.data.decode("utf-8"))
-
-        expected = {'result': 'success', "accepted": asdict(leader)}
-        self.assertEqual(actual, expected)
-
-# [GET] /subordinates
-    def test_get_subordinates_none(self):
-        response = self.app.get('/commander/subordinates')
-        self.assertEqual(response.status_code, 200)
-        actual = json.loads(response.data.decode("utf-8"))
-
-        # assert
-        expected = {'subordinates': []}
-        self.assertEqual(actual, expected)
-
-    def test_get_subordinates_single(self):
-        # add a leader
-        leader = LeaderInfo(id='lxxx0',
-                            name='cmd_http',
-                            endpoint='http://localhost:50000',
-                            subordinates=[],
-                            missions=[])
-        self.app.post('/commander/subordinates',
-                      data=json.dumps(asdict(leader)),
-                      content_type='application/json')
-
-        # get subordinates
-        response = self.app.get('/commander/subordinates')
-        self.assertEqual(response.status_code, 200)
-        actual = json.loads(response.data.decode("utf-8"))
-
-        # assert
-        expected = {'subordinates': [asdict(leader)]}
-        self.assertEqual(actual, expected)
-
-    def test_get_subordinates_multi(self):
-        # add some leader
-        leader_base = LeaderInfo(id='lxxx0',
-                                 name='cmd_http',
-                                 endpoint='http://localhost:50000',
-                                 subordinates=[],
-                                 missions=[])
-        leader_list = [
-            leader_base._replace(id='lxxx0'),
-            leader_base._replace(id='lxxx1'),
-            leader_base._replace(id='lxxx2'),
-            leader_base._replace(id='lxxx3'),
-        ]
-        for l in leader_list:
-            self.app.post('/commander/subordinates',
-                          data=json.dumps(asdict(l)),
-                          content_type='application/json')
-
-        # get subordinates
-        response = self.app.get('/commander/subordinates')
-        self.assertEqual(response.status_code, 200)
-        actual = json.loads(response.data.decode("utf-8"))
-        actual_list = actual['subordinates']
-
-        # assert
-        expected_list = [asdict(l) for l in leader_list]
-        self.assertEqual(len(actual_list), len(expected_list))
-        for exp in expected_list:
-            if exp not in actual_list:
-                self.fail('{0} is not found.'.format(exp))
-
-# [POST] /campaigns
-    def test_add_campaign(self):
-        # 各種パラメータの詳細が決まっていないため、暫定値を採用。
-        # 最終的には、API自体に無効な入力パラメータをハネる機能を搭載したうえで
-        # TODO: 無効値を確認する用のテストを作成すべき
-        campaign = Campaign(author='cxxx0',
-                            destination='mongoserv',
-                            place='S101',
-                            purpose='A great app',
-                            requirements='brightness sound',
-                            trigger='a trigger')
-        response = self.app.post('/commander/campaigns',
-                                 data=json.dumps(asdict(campaign)),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        actual = json.loads(response.data.decode("utf-8"))
-
-        expected = {'result': 'success', "accepted": asdict(campaign)}
         self.assertEqual(actual, expected)
 
 # [GET] /campaigns
@@ -207,3 +111,103 @@ class CommanderTestCase(unittest.TestCase):
         for exp in expected_list:
             if exp not in actual_list:
                 self.fail('{0} is not found.'.format(exp))
+        pass
+
+# [POST] /campaigns
+    def test_add_campaign(self):
+        # 各種パラメータの詳細が決まっていないため、暫定値を採用。
+        # 最終的には、API自体に無効な入力パラメータをハネる機能を搭載したうえで
+        # TODO: 無効値を確認する用のテストを作成すべき
+        campaign = Campaign(author='cxxx0',
+                            destination='mongoserv',
+                            place='S101',
+                            purpose='A great app',
+                            requirements='brightness sound',
+                            trigger='a trigger')
+        response = self.app.post('/commander/campaigns',
+                                 data=json.dumps(asdict(campaign)),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        actual = json.loads(response.data.decode("utf-8"))
+
+        expected = {'result': 'success', "accepted": asdict(campaign)}
+        self.assertEqual(actual, expected)
+
+# [GET] /subordinates
+    def test_get_subordinates_none(self):
+        response = self.app.get('/commander/subordinates')
+        self.assertEqual(response.status_code, 200)
+        actual = json.loads(response.data.decode("utf-8"))
+
+        # assert
+        expected = {'subordinates': []}
+        self.assertEqual(actual, expected)
+
+    def test_get_subordinates_single(self):
+        # add a leader
+        leader = LeaderInfo(id='lxxx0',
+                            name='cmd_http',
+                            endpoint='http://localhost:50000',
+                            subordinates=[],
+                            missions=[])
+        self.app.post('/commander/subordinates',
+                      data=json.dumps(asdict(leader)),
+                      content_type='application/json')
+
+        # get subordinates
+        response = self.app.get('/commander/subordinates')
+        self.assertEqual(response.status_code, 200)
+        actual = json.loads(response.data.decode("utf-8"))
+
+        # assert
+        expected = {'subordinates': [asdict(leader)]}
+        self.assertEqual(actual, expected)
+
+    def test_get_subordinates_multi(self):
+        # add some leader
+        leader_base = LeaderInfo(id='lxxx0',
+                                 name='cmd_http',
+                                 endpoint='http://localhost:50000',
+                                 subordinates=[],
+                                 missions=[])
+        leader_list = [
+            leader_base._replace(id='lxxx0'),
+            leader_base._replace(id='lxxx1'),
+            leader_base._replace(id='lxxx2'),
+            leader_base._replace(id='lxxx3'),
+        ]
+        for l in leader_list:
+            self.app.post('/commander/subordinates',
+                          data=json.dumps(asdict(l)),
+                          content_type='application/json')
+
+        # get subordinates
+        response = self.app.get('/commander/subordinates')
+        self.assertEqual(response.status_code, 200)
+        actual = json.loads(response.data.decode("utf-8"))
+        actual_list = actual['subordinates']
+
+        # assert
+        expected_list = [asdict(l) for l in leader_list]
+        self.assertEqual(len(actual_list), len(expected_list))
+        for exp in expected_list:
+            if exp not in actual_list:
+                self.fail('{0} is not found.'.format(exp))
+        pass
+
+# [POST] /subordinates
+    def test_add_subordinate(self):
+        leader = LeaderInfo(id='lxxx0',
+                            name='cmd_http',
+                            endpoint='http://localhost:50000',
+                            subordinates=[],
+                            missions=[])
+        response = self.app.post('/commander/subordinates',
+                                 data=json.dumps(asdict(leader)),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        actual = json.loads(response.data.decode("utf-8"))
+
+        expected = {'result': 'success', "accepted": asdict(leader)}
+        self.assertEqual(actual, expected)
+
