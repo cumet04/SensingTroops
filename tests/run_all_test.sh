ace6fbbfb65bc77ae6339ec423b1bac611e36325
medalhkr <medalhkr@gmail.com>
2016-06-22 15:43:06 +0900

    [fix] testスクリプトが失敗があっても全部実行するようになっていたのを修正

--- tests/run_all_test.sh
+++ tests/run_all_test.sh
@@ -5,8 +5,8 @@ expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 cd $cwd/..
 
 source venv/bin/activate
-echo -e "\n>> test recruiter unit"; python -m tests.recruiter_test && \
-echo -e "\n>> test commander unit"; python -m tests.commander_test && \
-echo -e "\n>> test leader unit"; python -m tests.leader_test && \
-echo -e "\n>> test soldier unit"; python -m tests.soldier_test && \
-echo -e "\n>> test integration"; python -m tests.integration_test
+echo -e "\n>> test recruiter unit" && python -m tests.recruiter_test && \
+echo -e "\n>> test commander unit" && python -m tests.commander_test && \
+echo -e "\n>> test leader unit" && python -m tests.leader_test && \
+echo -e "\n>> test soldier unit" && python -m tests.soldier_test && \
+echo -e "\n>> test integration" && python -m tests.integration_test
