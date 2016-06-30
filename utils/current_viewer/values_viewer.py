1ca89260421bdf3c0f62ff283a3643f291ddb6e9
medalhkr <medalhkr@gmail.com>
2016-06-30 10:09:21 +0900

    [change] mongoのデータ構造の変更に合わせてviewerを修正

--- utils/current_viewer/values_viewer.py
+++ utils/current_viewer/values_viewer.py
@@ -7,14 +7,13 @@ def get_collection():
 
 def get_random_data():
     col = get_collection()
-    search = {"values": {"$elemMatch": {"type": "random"}}}
+    search = {"data.type": "random"}
     filter = {"_id": 0}
     res = col.find(search, projection=filter)
 
     value_list = []
     for item in res:
         # センサデータのvaluesからtypeがrandomのものを見つけそのvalueを取り出す
-        values = item["values"]
-        v = [o["value"] for o in values if o["type"] == "random"][0]
+        v = item["data"]["value"]
         value_list.append([item["time"], v])
     return value_list
