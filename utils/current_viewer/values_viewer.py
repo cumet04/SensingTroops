c74c0cd8c5fa15c7cbd2857a9379cb0c2be8d4aa
medalhkr <medalhkr@gmail.com>
2016-07-01 11:46:54 +0900

    [change] データビューアを過去24時間分の表示に変更

--- utils/current_viewer/values_viewer.py
+++ utils/current_viewer/values_viewer.py
@@ -1,19 +1,26 @@
 import pymongo
+import datetime
 
 
 def get_collection():
     return pymongo.MongoClient("192.168.0.21")["test"]["troops"]
+    # return pymongo.MongoClient("localhost")["troops"]["values"]
 
 
 def get_values(purpose, place, type):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    limit_time = now - datetime.timedelta(days=1)
+
     col = get_collection()
     s_purpose = {"purpose": purpose}
     s_place = {"place": place}
     s_type = {"data.type": type}
+    s_time = {"time": {"$gt": limit_time}}
     search = {}
     search.update(s_purpose)
     search.update(s_place)
     search.update(s_type)
+    search.update(s_time)
 
     filter = {"_id": 0}
     res = col.find(search, projection=filter)
