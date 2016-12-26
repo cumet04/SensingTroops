81ae42690ac75b547937e21475eddde918b691b2
medalhkr <medalhkr@gmail.com>
2016-12-26 17:48:25 +0900

    [add] run.shに最小構成のコマンド群をコメントで追加

--- rpc_impl/run.sh
+++ rpc_impl/run.sh
@@ -18,6 +18,16 @@ do
     python -m troops.soldier --port $i > /tmp/soldier_${i:3:2}.log &
 done
 
+
+# python -m troops.recruiter > /tmp/recruiter.log &
+# sleep 0.1
+# python -m troops.commander --port 51000 > /tmp/commander_00.log &
+# sleep 0.1
+# python -m troops.leader --port 52000 > /tmp/leader_00.log &
+# sleep 0.1
+# python -m troops.soldier --port 53000 > /tmp/soldier_00.log &
+
+
 python -m viewer.viewer
 
 pkill python
