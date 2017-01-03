9ba191228ca731e4f4187c9057a63b295f64cc33
medalhkr <medalhkr@gmail.com>
2017-01-03 22:29:42 +0900

    [add] troopビューアを作成

--- rpc_impl/viewer/viewer.py
+++ rpc_impl/viewer/viewer.py
@@ -1,9 +1,76 @@
 import argparse
 import xmlrpc.client as xmlrpc_client
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 server = Flask(__name__)
 
+
+@server.route('/troop.html', methods=['GET'])
+def show_troops():
+    return render_template('troops_viewer.html')
+
+@server.route('/troop.json', methods=['GET'])
+def get_troops_json():
+    """
+    指定されたCommanderが保有する部隊情報をツリー用jsonで返す
+    ---
+    parameters:
+      - name: endpoint
+        in: query
+        required: true
+    """
+    endpoint = request.args.get('endpoint', default='', type=str)
+    if endpoint == '':
+        return "lack of parameter", 400
+
+    client = xmlrpc_client.ServerProxy(endpoint)
+    com = client.retrieve_troop_info()
+
+    root = {
+        "text": "Commander: {0} at {1}".format(com['id'], com['place']),
+        "icon": "troops commander",
+        "href": com['endpoint'],
+        "nodes": [
+            { "text": 'missions', 'nodes': []},
+            { "text": "subs", 'nodes': []},
+        ]
+    }
+    for m in com['missions'].values():
+        root['nodes'][0]['nodes'].append({
+            'text': str(m)
+        })
+    for lid, leader in com['subs'].items():
+        l_node = {
+            "text": "Leader: {0} at {1}".format(lid, leader['place']),
+            "icon": "troops leader",
+            "href": leader['endpoint'],
+            "nodes": [
+                { "text": "operations", 'nodes': []},
+                { "text": "subs", 'nodes': []},
+            ]
+        }
+        for op in leader['operations'].values():
+            l_node['nodes'][0]['nodes'].append({
+                'text': str(op)
+            })
+        for sid, soldier in leader['subs'].items():
+            s_node = {
+                "text": "Soldier: {0} at {1}".format(sid, soldier['place']),
+                "icon": "troops soldier",
+                "href": soldier['endpoint'],
+                "nodes": [
+                    { "text": "orders", 'nodes': []},
+                    { "text": 'weapons: {0}'.format(str(soldier['weapons']))},
+                ]
+            }
+            for o in soldier['orders'].values():
+                s_node['nodes'][0]['nodes'].append({
+                    'text': str(o)
+                })
+            l_node['nodes'][1]['nodes'].append(s_node)
+        root['nodes'][1]['nodes'].append(l_node)
+
+    return jsonify([root])
 
 @server.route('/xmlrpc/exec', methods=['GET', 'POST'])
 def exec_rpc():
